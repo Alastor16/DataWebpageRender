f517474b4d8577f6283d3d4beef255eae8f28c76
Alastor Guzman <47994840+Alastor16@users.noreply.github.com>
2023-12-08 12:19:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,26 +6,25 @@ car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 
 st.header('Datos disponibles')
 
-st.write(car_data) 
-
-st.header('Elige tu tipo de grafica')
-
 
 hist_button = st.button('Construir histograma') # crear un botón
 disper_button = st.button('Construye un grafico de dispersion') 
 
 
 if hist_button: # al hacer clic en el botón
-    st.write('Creación de un histograma para los tipos de cilindros')
+    st.write('Creación de un histograma para el conjunto de datos de precio de venta de coches')
+    options = st.multiselect('elige el dato a graficar', ['price', 'model_year','condition' ,'cylinders', 'fuel','odometer']) #para elegir que graficar
+    
             
-    fig = px.histogram(car_data, x="cylinders")
+    fig = px.histogram(car_data, x=options)
     st.plotly_chart(fig, use_container_width=True)
 
 if disper_button: # al hacer clic en el botón
-    st.write('Creación de un grafico de dispersion para el tipo ')
+    st.write('Creación de un grafico de dispersion para el consumo de gasolina')
             
-    fig = px.scatter(car_data, x="type")
+    fig = px.scatter(car_data, x="fuel")
     st.plotly_chart(fig, use_container_width=True)
+
 
  
 
